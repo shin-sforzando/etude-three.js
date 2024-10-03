139cb70fd058dbd58b9f294f1a40935706816b27
Shin'ichiro Suzuki <shin@sforzando.co.jp>
2024-10-03 19:37:50 +0900

    chore: :paperclip: add some recommended extensions

--- src/main.ts
+++ src/main.ts
@@ -13,16 +13,16 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.TorusKnotGeometry( 2, 0.5, 64, 16, 2, 3 );
+const material = new THREE.MeshNormalMaterial({});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 camera.position.z = 5;
 
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
