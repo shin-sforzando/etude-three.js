ca91196eec0ca308dc8dd968835348919f416ba1
Shin'ichiro Suzuki <shin@sforzando.co.jp>
2024-10-08 10:56:21 +0900

    chore: :paperclip: update dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  35,
   window.innerWidth / window.innerHeight,
   0.1,
   1000,
@@ -13,12 +13,16 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.TorusKnotGeometry( 2, 0.5, 64, 16, 2, 3 );
-const material = new THREE.MeshNormalMaterial({});
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
+scene.add(ambientLight);
+const pointLight = new THREE.PointLight(0xffffff, 0.9);
+scene.add(pointLight);
+const geometry = new THREE.TorusKnotGeometry(2, 0.5, 64, 16, 2, 3);
+const material = new THREE.MeshPhongMaterial({ color: 0xa22041 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-camera.position.z = 5;
+camera.position.z = 10;
 
 function animate() {
   mesh.rotation.x += 0.01;
